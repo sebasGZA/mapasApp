d319f1a797932f2e8b6af66ed88549aafa51d8e7
= <=>
2022-02-13 09:28:12 -0500

    Página Zoom-Range - Diseño y tarea

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .mapa-container{
+      height: 100%;
+      width: 100%;
+    }
+
+    .row{
+      background-color:white;
+      border-radius:5px;
+      bottom:50px;
+      left:50px;
+      padding:10px;
+      position:fixed;
+      z-index:999;
+    }
+  `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +28,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'mapa', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [-75.567, 6.217], // starting position [lng, lat]
+      zoom: 10 // starting zoom
+    });
   }
 
 }
